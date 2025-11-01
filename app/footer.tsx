3c69620b3a6f3bb4f731f63ff6c9a6c99c527307
Faiz Jauzah <mfaizjauzah24@gmail.com>
2025-11-01 15:09:22 +0700

    fix: update footer

--- app/footer.tsx
+++ app/footer.tsx
@@ -70,10 +70,11 @@ export function Footer() {
   return (
     <footer className="mt-24 border-t border-zinc-100 px-0 py-4 dark:border-zinc-800">
       <div className="flex items-center justify-between">
-        <a href="https://github.com/ibelick/nim" target="_blank">
+        <a href="https://github.com/faizjauzah/personal-portofolio" target="_blank">
           <TextLoop className="text-xs text-zinc-500">
-            <span>© 2024 Nim.</span>
-            <span>Built with Motion-Primitives.</span>
+            <span>© 2025 Faiz Jauzah.</span>
+            <span>Built with care and love.</span>
+            <span>Exited on what the future holds.</span>
           </TextLoop>
         </a>
         <div className="text-xs text-zinc-400">
